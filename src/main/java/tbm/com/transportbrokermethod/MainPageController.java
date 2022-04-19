e49b10959480eac46353f7394e1cf019759d9757
DanielGabrys <daniello.gab@gmail.com>
2022-04-19 08:47:50 +0200

    Gui optimal version

--- src/main/java/tbm/com/transportbrokermethod/MainPageController.java
+++ src/main/java/tbm/com/transportbrokermethod/MainPageController.java
@@ -19,21 +19,16 @@ import javafx.scene.layout.Pane;
 import javafx.util.Callback;
 
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.ResourceBundle;
+import java.util.*;
 
 import static java.lang.Integer.parseInt;
 
 public class MainPageController implements Initializable {
 
-    ObservableList<InputData> list = FXCollections.observableArrayList();
+
     int input_arr[][];
-    int counter =0;
     int X=0;
     int Y=0;
-    private ArrayList<String> rec_tem = new ArrayList<String>();
     int cur_pos_x;
     int cur_pos_y;
 
@@ -54,16 +49,6 @@ public class MainPageController implements Initializable {
     @FXML
     private TableView<ObservableList<String>> table_input = new TableView<>();
 
-    @FXML
-    private TableColumn<InputData,String> col;
-
-    @FXML
-    void cosik(MouseEvent event)
-    {
-
-
-    }
-
 
     @FXML
     private Pane grid_panel;
@@ -83,22 +68,23 @@ public class MainPageController implements Initializable {
 
         int size_rec = parseInt(receiver_input_field.getText());
         int size_sup = parseInt(supplier_input_field.getText());
+        //System.out.println(size_rec+" "+size_sup);
 
-        X=size_sup;
-        Y=size_rec;
-
-        int arr_size=Math.max(size_rec, size_sup);
-
+        double size = table_input.getPrefWidth()/(size_rec +1);
         input_arr = new int[size_sup][size_rec];
+        for(int i =0;i<size_sup;i++)
+        {
+            for(int j=0;j<size_rec;j++)
+            {
+                input_arr[i][j]=0;
+            }
+        }
 
-
-
-
-        //adding columns(receivers)
-        table_input.getColumns().clear();
-
-
+        //adding columns
         ArrayList<String> columnNames = new ArrayList<>();
+
+        table_input.getColumns().clear();
+        table_input.getItems().clear();
 
         for (int i = 0; i <= size_rec; i++)
         {
@@ -117,14 +103,13 @@ public class MainPageController implements Initializable {
             final int finalIdx = i;
             TableColumn<ObservableList<String>, String> column = new TableColumn<>(columnNames.get(i));
             column.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue().get(finalIdx)));
+            column.setPrefWidth(size);
 
             table_input.getColumns().add(column);
 
         }
 
-
         // adding rows
-        int counter =0;
         for (int i = 0; i < size_sup; i++)
         {
             ArrayList<String> a = new ArrayList<>();
@@ -133,16 +118,11 @@ public class MainPageController implements Initializable {
 
             for(int j=0;j<size_rec;j++)
             {
-                a.add(counter+"");
-                counter++;
+                a.add(0+"");
             }
-
-           // for(int j=0;j<a.size();j++)
-                //System.out.println(a.get(j));
 
             table_input.getItems().add(FXCollections.observableArrayList(a));
         }
-        //table_input.setItems(this.list);
 
         grid_panel.setVisible(true);
     }
@@ -150,6 +130,7 @@ public class MainPageController implements Initializable {
     @FXML
     void load_data(ActionEvent event)
     {
+        /*
         for (int i=0;i<table_input.getItems().size();i++)
         {
             System.out.println();
@@ -159,6 +140,18 @@ public class MainPageController implements Initializable {
             }
             System.out.println();
         }
+        */
+        System.out.println("DATA");
+        for (int i=0;i<table_input.getItems().size();i++)
+        {
+
+            for(int j=0;j<table_input.getColumns().size()-1;j++)
+            {
+                System.out.print(input_arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+
     }
 
     @FXML
@@ -184,15 +177,16 @@ public class MainPageController implements Initializable {
     void add_grid_value(ActionEvent event)
     {
 
-
             String value = grid_value.getText();
+
+            //output array
+            input_arr[cur_pos_x][cur_pos_y]=parseInt(value);
+
 
             ObservableList<String> a = table_input.getItems().get(cur_pos_x);
             a.set(cur_pos_y, value);
             System.out.println(cur_pos_x + " " + cur_pos_y);
             table_input.getItems().set(cur_pos_x, FXCollections.observableArrayList(a));
-
-
 
     }
 
