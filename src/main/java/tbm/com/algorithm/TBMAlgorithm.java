01d5f840c1cd4c2a07910090dc8938430f2794c9
Jakub <jakub.forys@opoczta.pl>
2022-06-03 17:37:40 +0200

    Finished TBM algorithm

--- src/main/java/tbm/com/algorithm/TBMAlgorithm.java
+++ src/main/java/tbm/com/algorithm/TBMAlgorithm.java
@@ -1,15 +1,17 @@
 package tbm.com.algorithm;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 //WE WSZYSTKIM ZASTOSOWAŁEM ,,NAWIGACJĘ" PIERWSZA WSPÓŁRZĘDNA DOSTAWCA, DRUGA WSPÓŁRZĘDNA ODBIORCA
 public class TBMAlgorithm
 {
     private static int[][] transportTable;      //tablica z wartościami przejazdów (,,pierwsza tablica")
-    private static int[][] optimizationTable;   //tablica ze wskaźnikami optymalności (,,druga tablica")
-    private static int[] alpha;                 //tablica z alfami
-    private static int[] beta;                  //tablica z betami
+    private static float[][] optimizationTable;   //tablica ze wskaźnikami optymalności (,,druga tablica")
+    private static float[] alpha;                 //tablica z alfami
+    private static float[] beta;                  //tablica z betami
+    private static float[] tempAlpha;
+    private static float[] tempBeta;
+
 
     //pomocnicza metoda sprawdzająca czy tablica transportowa została już wypełniona [NIE SPRAWDZA TRAS FIKCYJNYCH]
     private static boolean isTableFilled(boolean[][] t)
@@ -25,51 +27,47 @@ public class TBMAlgorithm
     }
 
     //profits powinno być chyba zależne od długości providera i recipients
-    public static void compute(int[] provider, int[] recipient, float[][] profits)
+    public static int[][] compute(int[] provider, int[] recipient, float[][] profits)
     {
         //wykonanie początkowych działań, które robi się tylko raz (odpowiada to punktom 1,2,3 z planu)
-        doStartingSetup(recipient, provider, profits);
+        doStartingSetup(provider, recipient, profits);
 
         while(true)
         {
-            //computeAlphasAndBetas();
-
-
-            int[] alpha = new int[profits.length];
-            int[] beta = new int[profits[0].length];
+            alpha = new float[profits.length+1];
+            beta = new float[profits[0].length+1];
 
             //nie jestem pewien czy te dwie pętle są konieczne, czy automatycznie
             // niezapełnione będą NaN, ale tak można mieć chyba pewność
             //(patrz funkcja findAlphaBeta)
             for(int i=0; i< alpha.length; i++)
             {
-                alpha[i] = Number.NaN;
+                alpha[i] = Float.NaN;
             }
 
             for(int i=0; i< beta.length; i++)
             {
-                beta[i] = Number.NaN;
+                beta[i] = Float.NaN;
             }
 
             findAlphaAndBeta(alpha, beta, profits);
 
-            //constructOptimizationTable();
-
-            int optimizationTable = new int[recipient.length][provider.length];
+            optimizationTable = new float[provider.length+1][recipient.length+1];
 
             //po wywołaniu tej funkcji mamy tabelę zapełnioną zerami czyli "x" i wartościami
-            ConstructOptTable(transportTable,alpha,beta,profits, optimizationTable);
+            ConstructOptTable(alpha,beta,profits);
 
-
-            //if(haveWeReachedOptimum()) break;
 
             //tutaj należy sprawdzić czy wszystko jest niedodatnie w tabeli optimization
-            //isOptimal(optimizationTable);
+            if(isOptimal())
+            {
+                break;
+            }
 
-            //applyCycle();
-
-            //petla powtorzenia zmiany tabeli
+            applyCycle();
         }
+
+        return transportTable;
     }
 
     private static void doStartingSetup(int[] provider, int[] recipient, float[][] profits)
@@ -77,10 +75,10 @@ public class TBMAlgorithm
         //krok 1A
 
         //ustalenie rozmiarów tablic
-        int currentStateProvider[provider.length+1];
-        int currentStateRecipient[recipient.length+1];
+        int currentStateProvider[] = new int[provider.length+1];
+        int currentStateRecipient[] = new int[recipient.length+1];
         transportTable = new int[provider.length+1][recipient.length+1]; //tabela transportów
-        boolean isFilled[provider.length+1][recipient.length+1];
+        boolean isFilled[][] = new boolean[provider.length+1][recipient.length+1];
 
         //obliczenie podaży fikcyjnego dostawcy
         int imaginaryProvider = 0;
@@ -96,21 +94,21 @@ public class TBMAlgorithm
         currentStateProvider[provider.length] = imaginaryProvider;
         System.arraycopy(recipient, 0, currentStateRecipient, 0, recipient.length);
         currentStateRecipient[recipient.length] = imaginaryRecipient;
-        Arrays.fill(isFilled, false);
+//!!     //Arrays.fill(isFilled, false);
 
         //krok 1B
 
         //zmienne pomocnicze: maksymalna wartość i odpowiadające jej współrzędne w tablicy oraz
         //zmienna służąca sprawdzeniu czy max w danej iteracji był już przypisany
         float maxValue = 0.F;
-        int maxValueCoordinates[2];
-        boolean wasMinValueAssigned;
+        int maxValueCoordinates[] = new int[2];
+        boolean wasMaxValueAssigned;
 
         //TU PRZY SPRAWDZANIU W DŁUGOŚCIACH MOŻNA DAĆ -1 I WTEDY NIE BĘDZIE SPRAWDZAŁ FIKCYJNYCH
         while(!isTableFilled(isFilled))
         {
             //znajdź maksymalny zysk dla komórki nie mającej wypełnienia
-            wasMinValueAssigned = false;
+            wasMaxValueAssigned = false;
             for(int i=0; i<profits.length; i++)
             {
                 for(int j=0; j<profits[0].length; j++)
@@ -119,12 +117,12 @@ public class TBMAlgorithm
                     if(!isFilled[i][j])
                     {
                         //przypadek dla pierwszego przypisania
-                        if(!wasMinValueAssigned)
+                        if(!wasMaxValueAssigned)
                         {
                             maxValue = profits[i][j];
                             maxValueCoordinates[0] = i;
                             maxValueCoordinates[1] = j;
-                            wasMinValueAssigned = true;
+                            wasMaxValueAssigned = true;
                         }
                         //przypadek dla znalezienia nowego maxa
                         else if(profits[i][j] > maxValue)
@@ -181,35 +179,35 @@ public class TBMAlgorithm
         //uzupełnienie kolumny fikcyjnego odbiorcy
         for(int i=0; i<transportTable.length; i++)
         {
-            if(!isFilled[i][recipent.length])
+            if(!isFilled[i][recipient.length])
             {
-                transportTable[i][recipent.length] = currentStateProvider[i];
-                currentStateRecipient[recipent.length] -= currentStateProvider[i];
+                transportTable[i][recipient.length] = currentStateProvider[i];
+                currentStateRecipient[recipient.length] -= currentStateProvider[i];
                 currentStateProvider[i] = 0;
-                isFilled[i][recipent.length] = true;
+                isFilled[i][recipient.length] = true;
             }
         }
     }
 
-    private static void findAlphaAndBeta(int[] alpha, int[] beta, int [][] profits)
+    private static void findAlphaAndBeta(float[] alpha, float[] beta, float[][] profits)
     {
         //zmienna pomocnicza do sprawdzania czy pole ma wartosc ktora mozna obliczyc alfę albo betę
-        public static boolean isValue = true;
+        boolean isValue = true;
 
 
-        public static int[] tempAlpha = Arrays.copyOf(alpha, alpha.length);
-        public static int[] tempBeta = Arrays.copyOf(beta, beta.length);
+        tempAlpha = Arrays.copyOf(alpha, alpha.length);
+        tempBeta = Arrays.copyOf(beta, beta.length);
 
         //sprawdzenie czy liczba jest "liczbą"
         for(int i=0; i< alpha.length; i++)
         {
-        if(Number.isNaN(alpha[i]))
-            isValue = false;
+            if(Float.isNaN(alpha[i]))
+                isValue = false;
         }
 
         for(int i=0; i< beta.length; i++)
         {
-            if(Number.isNaN(beta[i]))
+            if(Float.isNaN(beta[i]))
                 isValue = false;
         }
 
@@ -222,19 +220,21 @@ public class TBMAlgorithm
         //obliczenie wartosci
         for(int i=0; i< alpha.length; i++)
         {
-            for(j=0; j<beta.length; j++)
+            for(int j=0; j<beta.length; j++)
             {
-                if(profits[i][j]>0)
+                if(transportTable[i][j]>0)
                 {
                     //jesli beta nie jest numerem, a alfa jest
-                    if(Number.isNaN(beta[j]) && !Number.isNaN(alpha[i]))
+                    if(Float.isNaN(beta[j]) && !Float.isNaN(alpha[i]))
                     {
-                        beta[j] = profits[i][j] - alpha[i];
+                        if(j>=profits[0].length || i>=profits.length) beta[j] = 0.F - alpha[i];
+                        else beta[j] = profits[i][j] - alpha[i];
                     }
                     //odwrotna sytuacja
-                    else if(!Number.isNaN(beta[j]) && Number.isNaN(alpha[i]))
+                    else if(!Float.isNaN(beta[j]) && Float.isNaN(alpha[i]))
                     {
-                        alpha[i] = profits[i][j] - beta[j];
+                        if(i>=profits.length || j>=profits[0].length) alpha[i] = 0.F - beta[j];
+                        else alpha[i] = profits[i][j] - beta[j];
                     }
                 }
             }
@@ -244,7 +244,7 @@ public class TBMAlgorithm
         {
             for(int i=0; i< alpha.length; i++)
             {
-                if(Number.isNaN(alpha[i]))
+                if(Float.isNaN(alpha[i]))
                 {
                     alpha[i] = 0;
                     break;
@@ -252,45 +252,108 @@ public class TBMAlgorithm
             }
         }
 
-    //samowywołanie się funkcji
-    findAlphaAndBeta(tempAlpha, tempBeta, profits);
+        //samowywołanie się funkcji
+        //findAlphaAndBeta(tempAlpha, tempBeta);
+        findAlphaAndBeta(alpha, beta, profits);
     }
 
-    private static void ConstructOptTable(int[][] transportTable, int[] alpha, int[] beta, int[][] profits, int[][] optimizationTable)
+    private static void ConstructOptTable(float[] alpha, float[] beta, float[][] profits)
     {
         for(int i=0; i< transportTable.length; i++)
         {
             for(int j=0; j<transportTable[0].length; j++)
             {
                 //jeśli w tabeli transportów jest wartość, ustawia zero czyli "x"
-                if(!Number.isNaN(transportTable[i][i]))
+                //if(!Float.isNaN(transportTable[i][j]))
+                if(transportTable[i][j]!=0)
                 {
-                    optimizationTable[i][j] = 0;
+                    optimizationTable[i][j] = Float.NaN;
                 }
 
                 else
                 {
-                    optimizationTable[i][j] = profits[i][j] - alpha[i]- beta[j];
+                    optimizationTable[i][j] = transportTable[i][j] - alpha[i]- beta[j];
                 }
             }
         }
     }
-}
 
-private static void isOptimal(int[][] optimizationTable)
-{
-    for (int i = 0; i < optimizationTable.length; i++)
+    private static boolean isOptimal()
     {
-        for(int j = 0; j< optimizationTable[0].length; j++)
-        {
-            if(optimizationTable[i][j] >0)
-            {
-                //do nothing for now
-            }
-        }
+        //sprawdzenie czy wszystkie elementy tablicy są <= 0 [czy tablica jest optymalna]
+        for(int i=0; i<optimizationTable.length; i++)
+            for(int j=0; j<optimizationTable[0].length; j++)
+                if(optimizationTable[i][j] > 0) return false;
+        return true;
     }
 
-    break;
-}
+    private static void applyCycle()
+    {
+        float maxVal = 0.F;
+        int optimalPathCoordinates[][] = new int[4][2];
 
+        //znajdź największy element i jego współrzędne
+        for (int i = 0; i < optimizationTable.length; i++)
+        {
+            for(int j = 0; j< optimizationTable[0].length; j++)
+            {
+                if(!Float.isNaN(optimizationTable[i][j]) && optimizationTable[i][j] > maxVal)
+                {
+                    maxVal = optimizationTable[i][j];
+                    optimalPathCoordinates[0][0] = i;
+                    optimalPathCoordinates[0][1] = j;
+                }
+            }
+        }
+
+        //znajdowanie cyklu optymalizacyjnego w tabeli
+        //zmienna pomocnicza
+        boolean wasPathAssigned = false;
+        //ustalenie współrzędnych, które zawsze muszą być takie same
+        optimalPathCoordinates[1][0] = optimalPathCoordinates[0][0];
+        optimalPathCoordinates[3][1] = optimalPathCoordinates[0][1];
+        //iteracja po ,,pierwszym wymiarze"
+        for(int i=0; i<optimizationTable[0].length; i++)
+        {
+            if(i!=optimalPathCoordinates[0][1])
+            {
+                //znaleźliśmy x w tej linii
+                if(Float.isNaN(optimizationTable[optimalPathCoordinates[0][0]][i]))
+                {
+                    //iteracja po ,,drugim wymiarze"
+                    for(int j=0; j<optimizationTable.length; j++)
+                    {
+                        if(j!=optimalPathCoordinates[0][0])
+                        {
+                            //znaleźliśmy x w tej linii ; jeśli znaleźliśmy 2 x do cyklu, to trzeci musi mieć
+                            // już ustalone miejsce (jeśli go tam nie ma, to nie ma tam też cyklu)
+                            if(Float.isNaN(optimizationTable[j][i]) &&
+                                    Float.isNaN(optimizationTable[j][optimalPathCoordinates[0][1]]))
+                            {
+                                //ustalenie współrzędnych
+                                optimalPathCoordinates[1][1] = i;
+                                optimalPathCoordinates[2][0] = j;
+                                optimalPathCoordinates[2][1] = i;
+                                optimalPathCoordinates[3][0] = j;
+                                //przerwanie obu pętli
+                                wasPathAssigned = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            if(wasPathAssigned) break;
+        }
+
+        //zastosowanie cyklu
+        //znajdź min w minusach cyklu
+        int changeValue = Math.min(transportTable[optimalPathCoordinates[1][0]][optimalPathCoordinates[1][1]]
+                , transportTable[optimalPathCoordinates[3][0]][optimalPathCoordinates[3][1]]);
+        //odejmij od minusów i dodaj do plusów
+        transportTable[optimalPathCoordinates[0][0]][optimalPathCoordinates[0][1]] += changeValue;
+        transportTable[optimalPathCoordinates[1][0]][optimalPathCoordinates[1][1]] -= changeValue;
+        transportTable[optimalPathCoordinates[2][0]][optimalPathCoordinates[2][1]] += changeValue;
+        transportTable[optimalPathCoordinates[3][0]][optimalPathCoordinates[3][1]] -= changeValue;
+    }
 }
--- src/main/java/tbm/com/algorithm/algo_test.java
+++ src/main/java/tbm/com/algorithm/algo_test.java
@@ -2,5 +2,45 @@ package tbm.com.algorithm;
 
 public class algo_test
 {
+    public static void main(String args[])
+    {
+        /*
+        int provider[] = new int[3];
+        int recipient[] = new int[1];
+        float profits[][] = new float[3][1];
+        provider[0] = 13;
+        provider[1] = 54;
+        provider[2] = 10;
+        recipient[0] = 61;
+        profits[0][0] = 3.F;
+        profits[1][0] = 4.F;
+        profits[2][0] = 7.F;
+        */
 
+        int provider[] = new int[2];
+        int recipient[] = new int[3];
+        float profits[][] = new float[2][3];
+        provider[0] = 20;
+        provider[1] = 30;
+        recipient[0] = 10;
+        recipient[1] = 28;
+        recipient[2] = 27;
+        profits[0][0] = 12.F;
+        profits[0][1] = 1.F;
+        profits[0][2] = 3.F;
+        profits[1][0] = 6.F;
+        profits[1][1] = 4.F;
+        profits[1][2] = -1.F;
+
+
+        int[][] result = TBMAlgorithm.compute(provider, recipient, profits);
+        for(int i=0; i<result.length; i++)
+        {
+            for(int j=0; j<result[0].length; j++)
+            {
+                System.out.print(result[i][j]+" ");
+            }
+            System.out.print("\n");
+        }
+    }
 }
